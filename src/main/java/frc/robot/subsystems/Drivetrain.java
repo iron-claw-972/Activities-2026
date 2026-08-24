fd7d0a40ce7ceab74250f300fd7ced39132f59fd
deveshjain7577-dotcom <deveshjain7577@gmail.com>
2026-08-24 15:45:53 -0700

    Update Drivetrain.java

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,24 +1,28 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.controls.Follower;
+import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
 import frc.robot.constants.DriveConstants;
 
 public class Drivetrain extends SubsystemBase {
-  
+
   private CANSparkMax leftMotor1;
   private CANSparkMax leftMotor2;
   private CANSparkMax rightMotor1;
   private CANSparkMax rightMotor2;
 
   // TODO 2.1.1: Create DifferentialDrivetrainSim object (don't define it here)
+  private DifferentialDrivetrainSim driveSim;
 
   // TODO 2.2.1: Create gyro (AHRS)
 
@@ -27,7 +31,6 @@ public class Drivetrain extends SubsystemBase {
   // TODO 2.2.4: Create DifferentialDrivePoseEstimator
 
   // TODO 6.1.5: Create Feedforward and PIDs
-
 
   public Drivetrain() {
 
@@ -48,36 +51,46 @@ public class Drivetrain extends SubsystemBase {
     // TODO 1.2.4: Invert motors if necessary
 
     // TODO 2.1.1: Define DifferentialDrivetrainSim if the robot isn't real
-
+      if (RobotBase.isReal()){
+        driveSim= new DifferentialDrivetrainSim(DriveConstants.DRIVETRAIN_PLANT, DriveConstants.MOTOR, DriveConstants.GEAR_RATIO,DriveConstants.WHEEL_DIAMETER, DriveConstants.TRACK_WIDTH,DriveConstants.MEASUREMENT_STD_DEVS)
   }
-
-   /**
-   * This will be called every 20ms, or 50 times per second
-   */
-  @Override
-  public void periodic(){
-    // TODO 2.2.5: Update odometry
-    // TODO 1.2.2: Call tankDrive()
-    Robot.driver.getRightTranslation();
-    Robot.driver.getLeftTranslation();
-    // TODO 3.1.1: Remove all of the tank drive code in this method
-
-    // TODO 2.1.3: Update sim if in simulation
     
   }
 
   /**
-   * Drives the robot using tank drive controls. Tank drive is slightly easier to code but less
+   * This will be called every 20ms, or 50 times per second
+   */
+  @Override
+  public void periodic() {
+    // TODO 2.2.5: Update odometry
+    // TODO 1.2.2: Call tankDrive()
+    Robot.driver.getRightTranslation();
+    Robot.driver.getLeftTranslation();
+    if (RobotBase.isReal()){
+        driveSim= new DifferentialDrivetrainSim(DriveConstants.DRIVETRAIN_PLANT, DriveConstants.MOTOR, DriveConstants.GEAR_RATIO,DriveConstants.WHEEL_DIAMETER, DriveConstants.TRACK_WIDTH,DriveConstants.MEASUREMENT_STD_DEVS)
+  }
+    // TODO 3.1.1: Remove all of the tank drive code in this method
+
+    // TODO 2.1.3: Update sim if in simulation
+
+  }
+
+  /**
+   * Drives the robot using tank drive controls. Tank drive is slightly easier to
+   * code but less
    * intuitive to control than arcade drive.
    *
-   * @param leftPower the commanded power to the left motors (-1 to 1)
+   * @param leftPower  the commanded power to the left motors (-1 to 1)
    * @param rightPower the commanded power to the right motors (-1 to 1)
    */
   public void tankDrive(double leftPower, double rightPower) {
     // TODO 1.2.1: Implement tankDrive
-    double leftMotor1=leftPower;
-    double rightMotor1=rightPower;
+    double leftMotor1 = leftPower;
+    double rightMotor1 = rightPower;
     tankDrive(leftPower, rightPower);
+     if (RobotBase.isReal()){
+        driveSim= new DifferentialDrivetrainSim(DriveConstants.DRIVETRAIN_PLANT, DriveConstants.MOTOR, DriveConstants.GEAR_RATIO,DriveConstants.WHEEL_DIAMETER, DriveConstants.TRACK_WIDTH,DriveConstants.MEASUREMENT_STD_DEVS)
+  }
     // TODO 2.1.2: If in sim, set sim inputs
 
   }
@@ -86,51 +99,55 @@ public class Drivetrain extends SubsystemBase {
    * Drives the robot using arcade controls.
    *
    * @param forward the commanded forward movement
-   * @param turn the commanded turn rotation
+   * @param turn    the commanded turn rotation
    */
   public void arcadeDrive(double throttle, double turn) {
     // TODO 3.1.2: Implement arcadeDrive
-    
+
   }
 
-  public Pose2d getPose(){
+  public Pose2d getPose() {
     // TODO 2.2.6: Implement this method
     return new Pose2d();
   }
 
-  public void resetEncoders(){
+  public void resetEncoders() {
     // TODO 3.3.7: Reset encoders
 
   }
 
   // TODO 2.2.2: Implement these 4 methods
-  public double getLeftPosition(){
+  public double getLeftPosition() {
     return 0;
   }
-  public double getRightPosition(){
+
+  public double getRightPosition() {
     return 0;
   }
-  public double getAveragePosition(){
+
+  public double getAveragePosition() {
     return 0;
   }
-  public Rotation2d getGyroAngle(){
+
+  public Rotation2d getGyroAngle() {
     return null;
   }
 
-  public void tankDriveVolts(double left, double right){
+  public void tankDriveVolts(double left, double right) {
     // TODO 6.1.1: Implement this
 
   }
 
   // TODO 6.2.1: Implement these 2 methods
-  public double getLeftSpeed(){
-    return 0;
-  }
-  public double getRightSpeed(){
+  public double getLeftSpeed() {
     return 0;
   }
 
-  public void feedforwardDrive(double throttle, double turn){
+  public double getRightSpeed() {
+    return 0;
+  }
+
+  public void feedforwardDrive(double throttle, double turn) {
     // TODO 6.2.2: Create wheel speeds
 
     // TODO 6.2.3: Calculate voltages and call tankDriveVolts()
