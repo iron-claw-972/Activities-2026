0648edd7de945e593c6a64532fad0e7d2fcbd825
mayasureshsury-cmd <maya.suresh.sury@gmail.com>
2026-08-24 15:14:02 -0700

    Update NewSubsystem.java

--- src/main/java/frc/robot/subsystems/NewSubsystem.java
+++ src/main/java/frc/robot/subsystems/NewSubsystem.java
@@ -1,5 +1,11 @@
-package frc.robot.subsystems.NewSubsystem;
-public class NewSubsystem {
-    
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class NewSubsystem  extends SubsystemBase{
+     public static void main(String args[]){
+        
+
+}
     
 }
