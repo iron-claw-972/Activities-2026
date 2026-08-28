139ad40e0c9f60e4ffa09a1b118ef39110fc148b
DavidKaplunov-972 <drealkaplunov@gmail.com>
2026-08-28 14:46:52 -0700

    added drivetrain motors

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,10 +1,20 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.Voltage;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
+import frc.robot.constants.Constants;
+import frc.robot.constants.DriveConstants;
 
 public class Drivetrain extends SubsystemBase {
   
@@ -14,9 +24,9 @@ public class Drivetrain extends SubsystemBase {
   private CANSparkMax rightMotor2;
 
   // TODO 2.1.1: Create DifferentialDrivetrainSim object (don't define it here)
-
+DifferentialDrivetrainSim driveSim;
   // TODO 2.2.1: Create gyro (AHRS)
-
+  AHRS gyro = new AHRS(SPI.Port.kMXP);
   // TODO 2.2.3: Create DifferentialDriveKinematics
 
   // TODO 2.2.4: Create DifferentialDrivePoseEstimator
@@ -27,32 +37,53 @@ public class Drivetrain extends SubsystemBase {
   public Drivetrain() {
 
     // TODO 1.1.2: Initialize motors
+    CANSparkMax leftMotor1 = new CANSparkMax(DriveConstants.LEFT_MOTOR_1_ID, MotorType.kBrushless);    
+    CANSparkMax leftMotor2 = new CANSparkMax(DriveConstants.LEFT_MOTOR_2_ID, MotorType.kBrushless);
+    CANSparkMax rightMotor1 = new CANSparkMax(DriveConstants.RIGHT_MOTOR_1_ID, MotorType.kBrushless);
+    CANSparkMax rightMotor2 = new CANSparkMax(DriveConstants.RIGHT_MOTOR_2_ID,MotorType.kBrushless);
 
     // TODO 1.1.3: Set motors to brake mode
-  
+    leftMotor1.setIdleMode(IdleMode.kBrake);
+    leftMotor2.setIdleMode(IdleMode.kBrake);
+    rightMotor1.setIdleMode(IdleMode.kBrake);
+    rightMotor2.setIdleMode(IdleMode.kBrake);
     // TODO 1.1.4: Make motor2s follow motor1s
+    leftMotor1.follow(leftMotor2);      
+    rightMotor1.follow(rightMotor2);
+  // TODO 1.2.4: Invert motors if necessary
 
-    // TODO 1.2.4: Invert motors if necessary
+  // TODO 2.1.1: Define DifferentialDrivetrainSim if the robot isn't real
+    if (RobotBase.isSimulation()){
+      driveSim = new DifferentialDrivetrainSim(DriveConstants.MOTOR, DriveConstants.GEAR_RATIO, 4, 20, DriveConstants.WHEEL_DIAMETER/2, DriveConstants.TRACK_WIDTH/2, DriveConstants.MEASUREMENT_STD_DEVS);
 
-    // TODO 2.1.1: Define DifferentialDrivetrainSim if the robot isn't real
-
+    }
   }
-
-   /**
+   /*
    * This will be called every 20ms, or 50 times per second
    */
   @Override
   public void periodic(){
+
     // TODO 2.2.5: Update odometry
 
     // TODO 1.2.2: Call tankDrive()
+    tankDrive(Robot.driver.getLeftTranslation(), Robot.driver.getRightTranslation());
 
     // TODO 3.1.1: Remove all of the tank drive code in this method
 
     // TODO 2.1.3: Update sim if in simulation
-    
+    if (RobotBase.isSimulation()){
+      driveSim.update(Constants.LOOP_TIME);
+    }
   }
 
+  @Override
+  public void simulationPeriodic() {
+    if (!RobotBase.isReal()){
+      driveSim.update(Constants.LOOP_TIME);
+    }
+
+  }
   /**
    * Drives the robot using tank drive controls. Tank drive is slightly easier to code but less
    * intuitive to control than arcade drive.
@@ -62,11 +93,13 @@ public class Drivetrain extends SubsystemBase {
    */
   public void tankDrive(double leftPower, double rightPower) {
     // TODO 1.2.1: Implement tankDrive
-
-    // TODO 2.1.2: If in sim, set sim inputs
-
+    leftMotor1.set(leftPower*0.25);
+    rightMotor1.set(rightPower*0.25); 
+    if (RobotBase.isSimulation()){
+      driveSim.setInputs(leftPower*0.25*Constants.ROBOT_VOLTAGE, rightPower*0.25*Constants.ROBOT_VOLTAGE);
+    }
+    // TODO 2.1.2: If in sim, set sim inputs 
   }
-
   /**
    * Drives the robot using arcade controls.
    *
