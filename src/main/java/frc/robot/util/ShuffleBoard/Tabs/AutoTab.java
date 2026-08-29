051ba372c43e2c7b7507b30aaeb0e169bf908021
mayasureshsury-cmd <maya.suresh.sury@gmail.com>
2026-08-29 12:13:20 -0700

    new

--- src/main/java/frc/robot/util/ShuffleBoard/Tabs/AutoTab.java
+++ src/main/java/frc/robot/util/ShuffleBoard/Tabs/AutoTab.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
+import frc.robot.commands.AutoDriveCommand;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.util.ShuffleBoard.ShuffleBoardTabs;
 
@@ -22,9 +23,13 @@ public class AutoTab extends ShuffleBoardTabs {
     
         // Default auto
         autoCommand.setDefaultOption("Do Nothing", new PrintCommand("This will do nothing!"));
+
+    
         // Add auto commands here
         // TODO 3.2.7: Add your auto command here
-
+                autoCommand.addOption(
+            "Zigzag Auto",
+            new AutoDriveCommand(drivetrain));
         // TODO 3.3.8: Add your Bang-Bang drive command here
 
         // TODO 3.3.11: Add your Bang-Bang command for your subsystem here
